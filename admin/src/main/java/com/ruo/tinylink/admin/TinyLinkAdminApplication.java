3ee07467c230e5fc74d527ce58751d098335bccd
rlizzy <ruoliu.dev@gmail.com>
2024-05-04 20:29:51 -0500

    init: project structure

---
+++ admin/src/main/java/com/ruo/tinylink/admin/TinyLinkAdminApplication.java
@@ -0,0 +1,11 @@
+package com.ruo.tinylink.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyLinkAdminApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(TinyLinkAdminApplication.class, args);
+  }
+}
